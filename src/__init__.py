d004ae9465c3a7a6aea1ba6708b3ec60d5af0910
Hubert Chen <hubertchen@Huberts-iMac.hsd1.nj.comcast.net>
2024-06-21 21:44:31 -0400

    add user route

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 print('src init')
 from src.routes.friend_routes import friend_bp
+from src.routes.user_routes import user_bp
 
 
 
@@ -8,5 +9,6 @@ def create_app():
     app = Flask(__name__)
     print('create app')
     app.register_blueprint(friend_bp)
+    app.register_blueprint(user_bp)
     return app
 
